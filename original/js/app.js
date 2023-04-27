5d236b83f73dce7e6b3a1dce272f9d11c5a9b28b
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-04-27 03:36:10 +0300

    using data-* Attributes, adding Reset and New Round Buttons

--- original/js/app.js
+++ original/js/app.js
@@ -1,13 +1,23 @@
 const App = {
   // All our selected HTML Elements
   $: {
-    menu: document.querySelector('.menu'),
-    menuItems: document.querySelector('.items'),
+    menu: document.querySelector('[data-id="menu"'),
+    menuItems: document.querySelector('[data-id="menu-items"'),
+    resetBtn: document.querySelector('[data-id="reset-btn"'),
+    newRoundBtn: document.querySelector('[data-id="new-round-btn"'),
   },
 
   init() {
     App.$.menu.addEventListener('click', (event) => {
       App.$.menuItems.classList.toggle('hidden');
+    });
+
+    App.$.resetBtn.addEventListener('click', () => {
+      console.log('Reset the game');
+    });
+
+    App.$.newRoundBtn.addEventListener('click', () => {
+      console.log('Add a new Round');
     });
   },
 };
