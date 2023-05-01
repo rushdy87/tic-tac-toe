800a98b233c35bc372e46066ee7ed46d82fde1d7
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-05-01 05:47:38 +0300

    Sitting up Typescript

---
+++ ts-refactor/js/app.ts
@@ -0,0 +1,61 @@
+import Store from './store.js';
+import View from './view.js';
+
+// Our players "config" - defines icons, colors, name, etc.
+const players = [
+  {
+    id: 1,
+    name: 'Player 1',
+    iconClass: 'fa-x',
+    colorClass: 'turquoise',
+  },
+  {
+    id: 2,
+    name: 'Player 2',
+    iconClass: 'fa-o',
+    colorClass: 'yellow',
+  },
+];
+
+//
+function init() {
+  const view = new View();
+  const store = new Store('ttt-storge-key', players);
+
+  // Current tab state changes.
+  store.addEventListener('stateChange', () => {
+    view.render(store.game, store.stats);
+  });
+
+  // Different tab state changes.
+  window.addEventListener('storage', () => {
+    console.log('State changed from another tap');
+    view.render(store.game, store.stats);
+  });
+
+  // The first load of the document.
+  view.render(store.game, store.stats);
+
+  view.bindGameResetEvent((event) => {
+    store.reset();
+  });
+
+  view.bindNewRoundEvent((event) => {
+    store.newRound();
+  });
+
+  view.bindPlayerMoveEvent((square) => {
+    const existingMove = store.game.moves.find(
+      (move) => move.squareId === +square.id
+    );
+
+    if (existingMove) {
+      return;
+    }
+
+    // Advance to the next state by pushing the move to the moves array
+    store.playerMove(+square.id);
+  });
+}
+
+window.addEventListener('load', init);
