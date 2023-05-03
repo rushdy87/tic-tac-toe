4f6179b6d1ccd0596bfdfaa19fde1a104986bf86
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-05-03 05:42:30 +0300

    Initializing TypeScript in a React app

--- react-refactor/webpack.config.js
+++ react-refactor/webpack.config.js
@@ -2,29 +2,22 @@ const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV ?? 'development',
-  entry: './src/entery-point.js',
+  entry: './src/entery-point.tsx',
   module: {
     rules: [
       {
-        test: /.jsx?$/,
+        test: /.tsx?$/,
+        use: 'ts-loader',
         exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                '@babel/preset-env',
-                ['@babel/preset-react', { runtime: 'automatic' }],
-              ],
-            },
-          },
-        ],
       },
       {
-        test: /.css$/,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
     ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
     path: path.resolve(__dirname, 'public'),
